e0a81dbc8c546a666b9a2685e69c1542670b8a66
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-08-09 10:23:33 +0530

    images optmized

--- frontEnd/optimize-images.sh
+++ frontEnd/optimize-images.sh
@@ -3,11 +3,11 @@
 # Create a directory for optimized images if it doesn't exist
 mkdir -p ./public/assets/optimized
 
-# Loop through all image files in the assets directory
-for file in ./public/assets/*.{jpg,jpeg,png,webp}; do
-  # Get the base filename without the extension
-  filename=$(basename "$file" | cut -f 1 -d '.')
+# Loop through all image files in the non-optimized directory
+for file in ./public/assets/non-optimized/*.{jpg,jpeg,png,webp}; do
+  # Get the base filename with the extension
+  filename=$(basename "$file")
 
   # Resize the image to 50% of its original dimensions and reduce quality to 85%
-  magick "$file" -resize 50% -quality 85 "./public/assets/optimized/${filename}-optimized.${file##*.}"
+  magick "$file" -resize 50% -quality 85 "./public/assets/optimized/${filename}"
 done
