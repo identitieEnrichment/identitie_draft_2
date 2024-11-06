9220ae799205215ce17da0a774104e4d87083301
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-11-06 14:35:15 +0530

    stoack images changed on sectors

--- frontEnd/src/utils/constants.js
+++ frontEnd/src/utils/constants.js
@@ -3,115 +3,101 @@ export const allServicesData = [
     title: "Sales & Operation",
     Description:
       "We drive success through strategic planning and efficient execution via our Sales & Operation wing. We streamline sales processes and operational workflows to enhance performance and maximize results. Our focus on integrating innovative solutions and data-driven insights ensures optimal efficiency and growth, delivering exceptional value and operational excellence.",
-    img: "../assets/optimized/branding-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/branding-optimized.webp?alt=media&token=4a8d0284-5d94-49f8-95bb-f14f24538862",
+    img: "../assets/toOptimize/salesAndOperation.jpg",
+
   },
   {
     title: "Architecture & Interiors",
     Description:
       "At Identitie, we transform Architecture & Interiors into a canvas for sophisticated design, where every detail merges form and function. From serene retreats to dynamic workspaces, our projects blend artistry with practicality, setting new standards in personalized design. We craft iconic landmarks and intimate spaces that offer enduring comfort and inspire admiration for generations.",
-    img: "../assets/optimized/archi-optimized.jpeg",
-    webp: "../assets/optimized/archi.jpg",
+      img: "../assets/toOptimize/archi.jpg",
   },
   {
     title: "Art & Entertainment",
     Description:
       "Identitie's Art & Entertainment division renews the ordinary into a vibrant cultural kaleidoscope, blending visual arts, performing arts, and digital innovations into captivating multi-sensory experiences. From avant-garde exhibitions to interactive performances, our commitment to artistic integrity and audience engagement ensures every experience inspires, sparks conversations, and forges connections across borders.",
-    img: "../assets/optimized/realEstate-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/realEstate-optimized.webp?alt=media&token=9efc58cb-ac01-4970-9726-12dedd8d3318",
+    img: "../assets/toOptimize/art.jpg",
   },
   {
     title: "Banking & Finance",
     Description:
       "In Banking & Finance, Identitie is a pillar of financial strength, offering tailored banking solutions, sustainable investment strategies, and ethical wealth management. Our holistic approach combines risk mitigation with opportunity assessment, creating a resilient financial ecosystem that excels in uncertainty.",
-    img: "../assets/optimized/fullfillment-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/fullfillment-optimized.webp?alt=media&token=7e1ee88e-431a-4d92-98f0-ef60b49afc64",
+    img: "../assets/toOptimize/Banking.png",
   },
   {
     title: "Beauty & Skincare",
     Description:
       "In the realm of Beauty & Skincare, Identitie excels in cosmetic couture, offering meticulously crafted products and treatments. We feature an exclusive collection from around the world including South Korea, the global skincare capital; focusing on authenticity and efficacy. Through bespoke consultations and personalized treatments, we ensure your skincare journey leaves you with radiant, glowing skin.",
-    img: "../assets/optimized/eventManagment-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/eventManagment-optimized.webp?alt=media&token=99d7d10b-d7e3-45bd-8834-6a90ffa94e68",
+      img: "../assets/toOptimize/beauty.jpg",
+      
   },
   {
     title: "Branding & Marketing",
     Description:
       "With Identitie’s competency in Branding & Marketing, we create impactful brand architectures and messages that blend artistic finesse with market intelligence. We launch inspiring visual campaigns and develop cross-channel strategies that drive results, ensuring brands achieve global resonance and local relevance.  ",
-    img: "../assets/optimized/furniture-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/furniture-optimized.webp?alt=media&token=6cd8dd8d-f6ab-4c6f-906b-34dff2b2e469",
+    img: "../assets/toOptimize/brandingAndMarketing.jpg",
   },
   {
     title: "Education & Philanthropy",
     Description:
       "Identitie weaves Education & Philanthropy into a tapestry of opportunity and empowerment. We support holistic educational initiatives from early childhood to adult learning, believing knowledge drives societal change. Our strategic philanthropy empowers communities, leaving a lasting legacy of transformative impact.",
-    img: "../assets/optimized/archi-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/archi-optimized.webp?alt=media&token=30d25fbe-a93b-4c4c-ba4a-00e904501b9d",
+    img: "../assets/toOptimize/education.jpg",
   },
   {
     title: "Energy & Conservation",
     Description:
       "Through Identitie’s Energy & Conservation initiatives, we build a sustainable energy ecosystem that balances economic and environmental needs. By investing in clean energy and ecosystem restoration, we minimize ecological impact and promote renewable energy, inspiring a global shift toward a resilient future.",
-    img: "../assets/optimized/Marketing-optimized.jpg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/Marketing-optimized.webp?alt=media&token=0ca947c1-d7b0-4b21-a5e0-9417e54c8670",
+    img: "../assets/toOptimize/energy.jpg",
   },
 
   {
     title: "Food & Beverages",
     Description:
       "Experience farm-to-table excellence with Identitie’s fresh, locally sourced produce and sustainable ingredients. From organic grains and heirloom vegetables to gourmet sauces and preserves, our offerings include exotic spices, specialty oils, and decadent desserts, all crafted with authenticity and flavor.",
-    img: "../assets/optimized/importAndExport-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/importAndExport-optimized.webp?alt=media&token=26196ab4-fc8b-4889-8e63-a85ae0ef60be",
+    img: "../assets/toOptimize/food.jpg"
   },
 
   {
     title: "Fashion & Lifestyle",
     Description:
       "Identitie’s Fashion & Lifestyle department showcases on a dynamic fusion of style and substance. We celebrate the art of living stylishly with an exquisite blend of fashion-forward designs and lush lifestyle essentials.",
-    img: "../assets/optimized/trade-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/trade-optimized.webp?alt=media&token=13784ef1-b073-4bde-8deb-be3ff28297ff",
+    img: "../assets/toOptimize/fashion.jpg",
   },
   {
     title: "Furniture & Product Design",
     Description:
       "At Identitie, we craft innovative, artisanal furniture and products that blend craftsmanship with user experience. Our designs transform raw materials into stunning, functional pieces, including adaptable smart furniture and eco-friendly options. Our commitment to innovation shapes a harmonious environment and lifestyle.",
-    img: "../assets/optimized/informationTechnology-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/informationTechnology-optimized.webp?alt=media&token=132c1a26-ab73-41de-ab17-e1cf0b685aac",
+    img: "../assets/toOptimize/furniture.jpg",
   },
   {
     title: "Health & Wellness",
     Description:
       "Identitie cultivates holistic harmony that nurtures mind, body, and soul by addressing the interconnectedness of physical health, mental clarity, and emotional resilience. Through rejuvenating spa retreats, nutritional counseling, personalized fitness programs, therapeutic treatments, wellness coaching and many more tailored regimes; we empower individuals to live life to the fullest.",
-    img: "../assets/optimized/sales-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/sales-optimized.webp?alt=media&token=76448e3c-d4ee-440c-9add-d7c1f17e2edd",
+    img: "../assets/toOptimize/health.jpg",
   },
   {
     title: "Hospitality & Tourism",
     Description:
       "Experience luxury and cultural immersion with Identitie’s Hospitality & Tourism. We offer personalized services, from tailored travel itineraries and exclusive accommodations to immersive culinary experiences celebrating local flavors. Enjoy the art of food and wine while unwinding in comfort.",
-    img: "../assets/optimized/branding-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/branding-optimized.webp?alt=media&token=4a8d0284-5d94-49f8-95bb-f14f24538862",
+    img: "../assets/toOptimize/hospitality.jpg",
   },
   {
     title: "Pets & Tails",
     Description:
       "Canine Couture reigns supreme in Identitie’s Pets & Tails sector, where we pamper furry companions with premium products and personalized care. Our dedicated play areas offer interactive toys, shaded lounging, and supervision for safe socialization and exercise. From lavish grooming sessions to customized dietary plans, we make sure that you spoil your tail-wagging friends with the luxury and love that they deserve!",
-    img: "../assets/optimized/branding-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/branding-optimized.webp?alt=media&token=4a8d0284-5d94-49f8-95bb-f14f24538862",
+    img: "../assets/toOptimize/pet.jpg",
   },
   {
     title: "Production & Management",
     Description:
       " From meticulous planning to flawless execution, Identitie’s Production & Management team integrates innovative strategies and cutting-edge technology to optimize workflows and maintain high quality. We manage complex projects and daily operations, ensuring excellence and exceeding expectations.",
-    img: "../assets/optimized/branding-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/branding-optimized.webp?alt=media&token=4a8d0284-5d94-49f8-95bb-f14f24538862",
+    img: "../assets/toOptimize/production.jpg",
   },
   {
     title: "Real Estate & Infrastructure",
     Description:
       "Identitie’s Real Estate & Infrastructure sector specializes in creating value through strategic development and innovative solutions. From impactful real estate projects to comprehensive infrastructure planning, we ensure optimal growth and sustainability. Our expert management throughout each phase guarantees quality, efficiency, and lasting impact, fostering vibrant and enduring communities",
-    img: "../assets/optimized/branding-optimized.jpeg",
-    webp: "https://firebasestorage.googleapis.com/v0/b/identitie-d1dc6.appspot.com/o/branding-optimized.webp?alt=media&token=4a8d0284-5d94-49f8-95bb-f14f24538862",
+    img: "../assets/toOptimize/realEstate.jpg",
   },
 ];
 
